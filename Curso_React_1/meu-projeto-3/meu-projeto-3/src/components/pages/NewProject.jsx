191b5b15672c822179cb69940733dbcb99dd5445
GeovaneSilvaP <geovanesil45sil@gmail.com>
2025-11-01 16:13:36 -0300

    aula react

--- Curso_React_1/meu-projeto-3/meu-projeto-3/src/components/pages/NewProject.jsx
+++ Curso_React_1/meu-projeto-3/meu-projeto-3/src/components/pages/NewProject.jsx
@@ -1,12 +1,43 @@
 import styles from "./NewProject.module.css";
 import ProjectForm from "../project/ProjectForm";
+import { useNavigate } from "react-router-dom";
+
 
 function NewProject() {
+
+  const navigate = useNavigate();
+
+  function cratePost(projeto) {
+    
+    //initialize cost and services
+    projeto.cost = 0;
+    projeto.services = [];
+
+    fetch("http://localhost:5000/projects", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(projeto)
+    })
+    .then((resposta)=>{
+      resposta.json()
+    })
+    .then((data)=>{
+      console.log(data);
+      //redrect
+      navigate('/projects', { state: { message: 'Projeto criado com sucesso!' } })
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  }
+
   return (
     <div className={styles.newproject_container}>
       <h1>Criar Projeto</h1>
       <p>Crie um projeto para depois adicionar os serviços</p>
-      <ProjectForm btnText="Criar Projeto"/>
+      <ProjectForm handleSubmit={cratePost} btnText="Criar Projeto"/>
     </div>
   );
 }
--- Curso_React_1/meu-projeto-3/meu-projeto-3/src/components/project/ProjectForm.jsx
+++ Curso_React_1/meu-projeto-3/meu-projeto-3/src/components/project/ProjectForm.jsx
@@ -6,8 +6,9 @@ import SubmitButton from "../form/SubmitButton";
 
 import styles from "./ProjectForm.module.css";
 
-function ProjectForm({ btnText }) {
+function ProjectForm({ handleSubmit, btnText, projectData }) {
   const [categories, setCategories] = useState([]);
+  const [project, setProject] = useState(projectData || {});
 
   useEffect(() => {
     fetch("http://localhost:5000/categories", {
@@ -25,26 +26,51 @@ function ProjectForm({ btnText }) {
       });
   }, []);
 
+  const submit = (e) => {
+    e.preventDefault();
+    handleSubmit(project);
+  };
+
+  function handleChange(e) {
+    setProject({ ...project, [e.target.name]: e.target.value });
+  }
+
+  function handleCategory(e) {
+    setProject({ 
+      ...project,
+      category: {
+      id: e.target.value,
+      name: e.target.options[e.target.selectedIndex].text,
+    },
+   });
+  }
+
   return (
     <div>
-      <form className={styles.form}>
+      <form onSubmit={submit} className={styles.form}>
         <InputForm
           type="text"
           text="Nome do projeto"
           name="name"
           placeholder="Insira o nome do projeto"
+          handleOnChance={handleChange}
+          value={project.name ? project.name : ''}
         />
         <InputForm
           type="number"
           text="Orçamento do projeto"
           name="budget"
           placeholder="Insira o orçamento total"
+          handleOnChance={handleChange}
+          value={project.budget ? project.budget : ''}
         />
         <div>
           <Select
             name="category_id"
             text="Selecione a categoria"
             options={categories}
+            handleOnChance={handleCategory}
+            value={project.category ? project.category.id : ''}
           />
         </div>
         <div>

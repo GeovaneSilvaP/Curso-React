06ddb1bddee5cfc2078ecd33f9d679da26a47369
GeovaneSilvaP <geovanesil45sil@gmail.com>
2025-11-03 21:49:57 -0300

    Projeto react

---
+++ 4_Cauculadora_IMC/vite-project/src/components/ImcCalc.jsx
@@ -0,0 +1,41 @@
+import "./ImcCal.css";
+
+import Button from "./Button";
+
+const ImcCalc = () => {
+  return (
+    <div id="calc-container">
+      <h2>Calculadora de IMC</h2>
+      <form id="imc-form">
+        <div className="form-input">
+          <div className="form-control">
+            <label htmlFor="height">Altura:</label>
+            <input
+              type="text"
+              name="height"
+              id="height"
+              placeholder="Exemplo 1,75"
+            />
+          </div>
+          <div className="form-input">
+            <div className="form-control">
+              <label htmlFor="weight">Peso:</label>
+              <input
+                type="text"
+                name="weight"
+                id="weight"
+                placeholder="Exemplo 70.5"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="action-control">
+          <Button id="calc-btn" text="Calcular" />
+          <Button id="clear-btn" text="Limpar" />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default ImcCalc;

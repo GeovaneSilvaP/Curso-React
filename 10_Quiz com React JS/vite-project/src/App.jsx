7a0b56c9a5bc61f2946d9be91d09df56553e50fc
GeovaneSilvaP <geovanesil45sil@gmail.com>
2025-11-15 09:56:58 -0300

    Projeto React

---
+++ 10_Quiz com React JS/vite-project/src/App.jsx
@@ -0,0 +1,27 @@
+//react, componentes, estático
+import { useContext, useEffect } from "react";
+import { QuizContext } from "./context/quiz";
+
+import Welcome from "./components/Welcome";
+import Question from "./components/Question";
+
+import "./App.css";
+
+function App() {
+  const [quizState, dispatch] = useContext(QuizContext);
+
+  useEffect(() => {
+    //embaralhar as perguntas
+    dispatch({ type: "REORDER_QUESTIONS" });
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Quiz de Programação</h1>
+      {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Playing" && <Question />}
+    </div>
+  );
+}
+
+export default App;
